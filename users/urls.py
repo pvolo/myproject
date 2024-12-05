2ffdc46ad936ff11e99e4e93ec9d6078a760a8d6
pvolo <riv.salamanca@duocuc.cl>
2024-12-05 00:28:53 -0300

    first commit

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
